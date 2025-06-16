108a15e5cce584e495d3c4db5402807cf2b5362f
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-16 19:19:08 +0500

    added custom errors middlewares and wired every middlewares
    added custom errors middlewares and wired every middlewares

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-err';
 
 const app = express();
 app.use(json());
@@ -12,6 +14,22 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all(/(.*)/, async () => {
+  throw new NotFoundError();
+});
+
+// Error handling middleware must come after all routes
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
