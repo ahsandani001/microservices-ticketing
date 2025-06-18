29ad968c8cafb04f5f7e83bbe526b133ae2e0a95
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-18 22:52:58 +0500

    added signup unit tests

---
+++ auth/src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-err';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all(/(.*)/, async () => {
+  throw new NotFoundError();
+});
+
+// Error handling middleware must come after all routes
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
+
+export { app };
