09e2cc6ca1b85d912b2b836f2920a8fed44930fc
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-18 22:54:01 +0500

    Implemented signup unit tests (Release)
    * added basic routes and validated signup
    
    * added custom errors middlewares and wired every middlewares 
    
    * added signup for user 
    
    * added new custom error middleware
    
    * added password hashing 
    
    * added jwt and cookies for signup
    
    * added environment variables in kubernets
    
    * transformed document data from model
    
    * added signin flow 
    
    * implemented current user route 
    
    * implemented signout 
    
    * implemented separate nodeport service to connect mongoDB for mongodb compass
    
    * added signup unit tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,45 +1,5 @@
-import express from 'express';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-err';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all(/(.*)/, async () => {
-  throw new NotFoundError();
-});
-
-// Error handling middleware must come after all routes
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    errorHandler(err, req, res, next);
-  }
-);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
