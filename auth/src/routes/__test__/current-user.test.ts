73d34fb500eb7e88e6ede91f7acc1cdcb5d7e405
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-19 14:12:34 +0500

    implemented current-user unit tests (Release)
    * added basic routes and validated signup
    
    * added custom errors middlewares and wired every middlewares 
    
    * added signup for user 
    
    * added new custom error middleware
    
    * added password hashing 
    
    * added jwt and cookies for signup
    
    * added environment variables in kubernets
    
    * transformed document data from model
    
    * added signin flow 
    
    * implemented current user route 
    
    * implemented signout 
    
    * implemented separate nodeport service to connect mongoDB for mongodb compass
    
    * added signup unit tests
    
    * added signin unit tests
    
    * implemented signout unit tests 
    
    * implemented current-user unit tests

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about the current user', async () => {
+  const authResponse = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+  const cookie = authResponse.get('Set-Cookie');
+
+  if (cookie) {
+    const res = await request(app)
+      .get('/api/users/currentuser')
+      .set('Cookie', cookie)
+      .send({})
+      .expect(200);
+
+    expect(res.body.currentUser.email).toEqual('test@test.com');
+  }
+});
