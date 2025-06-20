4aea062560bdf081f276abfd812572e6d0189ff1
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-20 17:19:58 +0500

    Implemented initialProps on server side and client with completing signup (Release)
    * added basic routes and validated signup
    
    * added custom errors middlewares and wired every middlewares 
    
    * added signup for user 
    
    * added new custom error middleware
    
    * added password hashing 
    
    * added jwt and cookies for signup
    
    * added environment variables in kubernets
    
    * transformed document data from model
    
    * added signin flow 
    
    * implemented current user route 
    
    * implemented signout 
    
    * implemented separate nodeport service to connect mongoDB for mongodb compass
    
    * added signup unit tests
    
    * added signin unit tests
    
    * implemented signout unit tests 
    
    * implemented current-user unit tests
    
    * implemented global function to reduce redundency
    
    * initial commit - setting up next app and wired skaffold and ingress for routing
    
    * added config to watch files to show change immediately
    
    * implemented signup page with connected its api
    
    * Implemented initialProps on server side and client with completing signup

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,3 +1,25 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on server
+    // http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // we are on browser
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+};
+export default LandingPage;
