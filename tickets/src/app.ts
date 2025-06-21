3b8e64849dcea935211cf0d1d0e1a210c81fa910
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-21 18:21:20 +0500

    Tickets initial setup

---
+++ tickets/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError } from '@tickets-packages/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all(/(.*)/, async () => {
+  throw new NotFoundError();
+});
+
+// Error handling middleware must come after all routes
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
+
+export { app };
