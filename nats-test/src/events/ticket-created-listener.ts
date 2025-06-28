ad94b0786df6bfbe09338c0b2c75724b23032d2c
Ahsan Danish <ahsan.danish7427@gmail.com>
2025-06-28 15:03:11 +0500

    added interfaces to listener to strongly avoid typos for listener

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './tickets-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
     console.log('Event Data!', data);
 
     msg.ack();
