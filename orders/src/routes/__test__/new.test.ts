0e4cc245a82b548ccfbf4c4ea0383a069fd3af60
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-06-30 17:24:23 +0500

    implemented new order route with  models & unit tests (Release)
    * added basic routes and validated signup
    
    * added custom errors middlewares and wired every middlewares 
    
    added custom errors middlewares and wired every middlewares
    
    * added signup for user 
    
    added signup for user
    
    * added new custom error middleware
    
    * added password hashing 
    
    added password hashing
    
    * added jwt and cookies for signup
    
    * added environment variables in kubernets
    
    * transformed document data from model
    
    * added signin flow 
    
    added signin flow
    
    * implemented current user route 
    
    implemented current user route
    
    * implemented signout 
    
    implemented signout
    
    * implemented separate nodeport service to connect mongoDB for mongodb compass
    
    * added signup unit tests
    
    * added signin unit tests
    
    * implemented signout unit tests 
    
    implemented signout unit tests
    
    * implemented current-user unit tests
    
    * implemented global function to reduce redundency
    
    * initial commit - setting up next app and wired skaffold and ingress for routing
    
    * added config to watch files to show change immediately
    
    * implemented signup page with connected its api
    
    * Implemented initialProps on server side and client with completing signup
    
    * refactored getting initial props by adding a new file and getting api from client
    
    * refactored initial props for header
    
    * complete header
    
    * completed user signout
    
    * updated dependency and code with files
    
    * added separate npm module to share code
    
    * Tickets initial setup
    
    * added tickets deployment, clusterIP service & Nodeport service to connect in mongoDB compass
    
    * added skaffold to automate tickets kubernetes cluster
    
    * added env variable for auth mongo and update the deployment file to initialize env
    
    * added new middleware and error
    
    * added remaining exports
    
    * Add post route with unit tests
    
    * added the sample and unit tests based on that with authentication too
    
    * added route with completion of post route of ticket
    
    * added get ticket route with its unit tests
    
    * added get tickets route with its unit tests
    
    * implemented update ticket route with its unit tests
    
    * added nats-streaming deployment with its service
    
    * tested different properties of nats streaming server
    
    * added parent and child class for listener
    
    * refactored listener classes
    
    * cleaned listener file
    
    * added interfaces to listener to strongly avoid typos for listener
    
    * added interfaces to publisher to strongly avoid typos for publisher
    
    * added publisher and listener to common module
    
    * updated package.json file with updated common module version and implemented in tickets
    
    * updated userId in ticket created event interface
    
    * implementing nats in tickets
    
    * implementing nats and publish new ticket adding
    
    * implemented nats in update ticket to publish event in update ticket
    
    * updated other unit tests with implementation of nats and also updated env variables for nats
    
    * initialized orders service, created docker image, deployments, service, orders mongo, nodeport service and lastly skaffolded and update ingress
    
    * implemented initial routes for order service
    
    * added order status in common module
    
    * implemented new order route with  models & unit tests

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '../../models/order';
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  const ticket = await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId });
+
+  expect(ticket.status).toBe(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'asdfasdfasdf',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
+
+it.todo('emits an order created event');
