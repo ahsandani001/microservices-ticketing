32431faee7ac5b1a57d09478cd48c1039d75eb24
Ahsan Danish <ahsan.danish7427@gmail.com>
2025-07-01 17:48:30 +0500

    added listeners to ticket create and update event to make all records same

---
+++ orders/src/events/publishers/listeners/ticket-updated-listener.ts
@@ -0,0 +1,29 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  TicketUpdatedEvent,
+} from '@tickets-packages/common';
+import { Ticket } from '../../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName: string = queueGroupName;
+  async onMessage(
+    data: { id: string; title: string; price: number; userId: string },
+    msg: Message
+  ) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
