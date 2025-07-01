227c12cc6a8720695e7e27508faba76bc2107f5b
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-07-01 19:52:23 +0500

    added version in tickets model in tickets, orders and common with its unit tests to handle concurrency issue (Release)
    * added basic routes and validated signup
    
    * added custom errors middlewares and wired every middlewares 
    
    added custom errors middlewares and wired every middlewares
    
    * added signup for user 
    
    added signup for user
    
    * added new custom error middleware
    
    * added password hashing 
    
    added password hashing
    
    * added jwt and cookies for signup
    
    * added environment variables in kubernets
    
    * transformed document data from model
    
    * added signin flow 
    
    added signin flow
    
    * implemented current user route 
    
    implemented current user route
    
    * implemented signout 
    
    implemented signout
    
    * implemented separate nodeport service to connect mongoDB for mongodb compass
    
    * added signup unit tests
    
    * added signin unit tests
    
    * implemented signout unit tests 
    
    implemented signout unit tests
    
    * implemented current-user unit tests
    
    * implemented global function to reduce redundency
    
    * initial commit - setting up next app and wired skaffold and ingress for routing
    
    * added config to watch files to show change immediately
    
    * implemented signup page with connected its api
    
    * Implemented initialProps on server side and client with completing signup
    
    * refactored getting initial props by adding a new file and getting api from client
    
    * refactored initial props for header
    
    * complete header
    
    * completed user signout
    
    * updated dependency and code with files
    
    * added separate npm module to share code
    
    * Tickets initial setup
    
    * added tickets deployment, clusterIP service & Nodeport service to connect in mongoDB compass
    
    * added skaffold to automate tickets kubernetes cluster
    
    * added env variable for auth mongo and update the deployment file to initialize env
    
    * added new middleware and error
    
    * added remaining exports
    
    * Add post route with unit tests
    
    * added the sample and unit tests based on that with authentication too
    
    * added route with completion of post route of ticket
    
    * added get ticket route with its unit tests
    
    * added get tickets route with its unit tests
    
    * implemented update ticket route with its unit tests
    
    * added nats-streaming deployment with its service
    
    * tested different properties of nats streaming server
    
    * added parent and child class for listener
    
    * refactored listener classes
    
    * cleaned listener file
    
    * added interfaces to listener to strongly avoid typos for listener
    
    * added interfaces to publisher to strongly avoid typos for publisher
    
    * added publisher and listener to common module
    
    * updated package.json file with updated common module version and implemented in tickets
    
    * updated userId in ticket created event interface
    
    * implementing nats in tickets
    
    * implementing nats and publish new ticket adding
    
    * implemented nats in update ticket to publish event in update ticket
    
    * updated other unit tests with implementation of nats and also updated env variables for nats
    
    * initialized orders service, created docker image, deployments, service, orders mongo, nodeport service and lastly skaffolded and update ingress
    
    * implemented initial routes for order service
    
    * added order status in common module
    
    * implemented new order route with  models & unit tests
    
    * implemented get orders ruter with its unit tests
    
    * implemented individual route with its unit tests
    
    * implemented cancelling order route with its unit tests
    
    * added order created and cancelled events to publish
    
    * implemented publishing order creation and deletion events with its unit tests
    
    * added listeners to ticket create and update event to make all records same at Tickets and Orders DB
    
    * added version in tickets model in tickets, orders and common with its unit tests t handle concurrency issue

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -8,6 +8,7 @@ export interface OrderCreatedEvent {
     status: OrderStatus;
     userId: string;
     expireAt: string;
+    version: number;
     ticket: {
       id: string;
       // title: string;
