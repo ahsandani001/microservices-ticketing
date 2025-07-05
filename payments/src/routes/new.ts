fc0d0ca594cdfc7fdde54935c4244686f07f9bf2
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2025-07-05 17:49:40 +0500

    impleented stripe payments to store with its unit tests in payments service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@tickets-packages/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payments';
 
 const router = express.Router();
 
@@ -31,11 +32,17 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
 
     // await stripe.paymentIntents.create()
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100,
       source: token,
     });
+
+    const payment = await Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
