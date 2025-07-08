8ef7c46d12059a54b5cc10ffe925f9610cc1a0d0
Ahsan Danish <ahsan.danish7427@gmail.com>
2025-07-08 16:32:05 +0500

    added several workflows of services and updated unit tests

--- payments/src/__mocks__/stripe.ts
+++ payments/src/__mocks__/stripe.ts
@@ -2,6 +2,16 @@ const Stripe = jest.fn(() => ({
   charges: {
     create: jest.fn().mockResolvedValue({}),
   },
+  paymentIntents: {
+    create: jest.fn().mockResolvedValue({ id: 'pi_3RiZ2qRkuqjUGdRE0d9ZWKR4' }),
+    list: jest
+      .fn()
+      .mockResolvedValue({
+        data: [
+          { id: 'pi_3RiZ2qRkuqjUGdRE0d9ZWKR4', amount: 1000, currency: 'usd' },
+        ],
+      }),
+  },
 }));
 
 export = Stripe;
